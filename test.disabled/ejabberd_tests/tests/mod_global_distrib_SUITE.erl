a2a374a5860c912f2512d0095fa30ecd748d7f6d
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-02-28 16:37:55 +0100

    Make true value a default one

--- test.disabled/ejabberd_tests/tests/mod_global_distrib_SUITE.erl
+++ test.disabled/ejabberd_tests/tests/mod_global_distrib_SUITE.erl
@@ -249,13 +249,13 @@ generic_end_per_testcase(CaseName, Config) ->
 %%--------------------------------------------------------------------
 
 test_host_refreshing(_Config) ->
-    wait_until(fun() -> trees_for_connections_present() end, true, 2, ?HOSTS_REFRESH_INTERVAL),
+    wait_until(fun() -> trees_for_connections_present() end, 2, ?HOSTS_REFRESH_INTERVAL),
     ConnectionSups = out_connection_sups(asia_node),
     {europe_node1, EuropeHost, _} = lists:keyfind(europe_node1, 1, get_hosts()),
     EuropeSup = rpc(asia_node, mod_global_distrib_utils, server_to_sup_name, [list_to_binary(EuropeHost)]),
     {_, EuropePid, supervisor, _} = lists:keyfind(EuropeSup, 1, ConnectionSups),
     erlang:exit(EuropePid, kill),
-    wait_until(fun() -> tree_for_sup_present(asia_node, EuropeSup) end, true, 2, ?HOSTS_REFRESH_INTERVAL).
+    wait_until(fun() -> tree_for_sup_present(asia_node, EuropeSup) end, 2, ?HOSTS_REFRESH_INTERVAL).
 
 
 test_pm_between_users_at_different_locations(Config) ->
@@ -773,6 +773,9 @@ listen_endpoint(Port) ->
 rpc(NodeName, M, F, A) ->
     Node = ct:get_config(NodeName),
     ct_rpc:call(Node, M, F, A).
+
+wait_until(Predicate, Attempts, Sleeptime) ->
+    wait_until(Predicate, true, Attempts, Sleeptime).
 
 wait_until(Fun, ExpectedValue, Attempts, SleepTime) ->
     wait_until(Fun, ExpectedValue, Attempts, SleepTime, []).
